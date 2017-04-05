aaec8f59b3a2a5981b2ffb6f885911605398d6b1
benatespina <benatespina@gmail.com>
2017-04-05 14:34:28 +0200

    Added zip code

---
+++ src/LIN3S/SharedKernel/Domain/Model/ZipCode/ZipCode.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Shared Kernel library.
+ *
+ * Copyright (c) 2016-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LIN3S\SharedKernel\Domain\Model\ZipCode;
+
+use Uvinum\ZipCode\Validator;
+
+/**
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class ZipCode
+{
+    private $zipCode;
+
+    public static function fromSpain($zipCode)
+    {
+        return new self($zipCode, 'ES');
+    }
+
+    public function __construct($zipCode, $iso)
+    {
+        $this->setZipCode($zipCode, $iso);
+    }
+
+    public function zipCode()
+    {
+        return $this->zipCode;
+    }
+
+    public function __toString()
+    {
+        return (string) $this->zipCode();
+    }
+
+    private function setZipCode($zipCode, $iso)
+    {
+        $zipCode = (string) $zipCode;
+
+        $this->checkZipCodeIsCorrect($zipCode, $iso);
+        $this->zipCode = $zipCode;
+    }
+
+    private function checkZipCodeIsCorrect($zipCode, $iso)
+    {
+        if ((new Validator())->validate($iso, $zipCode)) {
+            throw new ZipCodeInvalidException();
+        }
+    }
+}
---
+++ src/LIN3S/SharedKernel/Domain/Model/ZipCode/ZipCodeInvalidException.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Shared Kernel library.
+ *
+ * Copyright (c) 2016-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LIN3S\SharedKernel\Domain\Model\ZipCode;
+
+use LIN3S\SharedKernel\Exception\Exception;
+
+/**
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class ZipCodeInvalidException extends Exception
+{
+    public function __construct($zipCode, $iso)
+    {
+        parent::__construct(
+            sprintf(
+                'The "%s" is not a valid zip code for "%s" ISO code',
+                $zipCode,
+                $iso
+            )
+        );
+    }
+}
